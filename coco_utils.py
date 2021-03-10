49c0d37c71a745de5c2b2d2564a6c437fe45effd
Pirarucu <pirarucugaming@gmail.com>
2021-03-10 18:00:25 -0300

    fixed

--- coco_utils.py
+++ coco_utils.py
@@ -29,24 +29,6 @@ class FilterAndRemapCocoCategories(object):
         target["annotations"] = anno
         return image, target
 
-
-def convert_coco_poly_to_mask(segmentations, height, width):
-    masks = []
-    for polygons in segmentations:
-        rles = coco_mask.frPyObjects(polygons, height, width)
-        mask = coco_mask.decode(rles)
-        if len(mask.shape) < 3:
-            mask = mask[..., None]
-        mask = torch.as_tensor(mask, dtype=torch.uint8)
-        mask = mask.any(dim=2)
-        masks.append(mask)
-    if masks:
-        masks = torch.stack(masks, dim=0)
-    else:
-        masks = torch.zeros((0, height, width), dtype=torch.uint8)
-    return masks
-
-
 class ConvertCocoPolysToMask(object):
     def __call__(self, image, target):
         w, h = image.size
@@ -242,10 +224,11 @@ def get_coco(root, image_set, transforms, mode='instances'):
 
     return dataset
 
-def get_custom(root, image_set, transforms, mode='instances'):
+def get_custom(root, image_set, transforms, mode='keypoints'):
     PATHS = {
         "train": ("train", os.path.join("annotations", "train.json")),
         "val": ("valid", os.path.join("annotations", "valid.json")),
+        "test": ("test", os.path.join("annotations", "test.json")),
     }
 
     t = [ConvertCocoPolysToMask()]
